0aed1c17e307cb13d25857e25f948abb8e535d7d
Kyle <kyleham@byu.edu>
2021-01-30 17:29:30 -0700

    Add revert file and editing insert mockData

--- insertMockData.sql
+++ insertMockData.sql
@@ -31,26 +31,3 @@ INSERT INTO public.view(
 INSERT INTO public.warehouse(
     storeid, storezip
 )    VALUES (1, 84604)
-_________________________________
-DELETE FROM public.admin(
-    WHERE adminid = 1);
-DELETE FROM public.category(
-    WHERE categoryid= 1);
-DELETE FROM public.contains(
-    WHERE storeid = 1);
-DELETE FROM public.customer(
-    WHERE customerid = 1);
-DELETE FROM public.employee(
-    WHERE employeeid = 1);
-DELETE FROM public.product(
-    WHERE productid = 1);
-DELETE FROM public.region(
-    WHERE regionid = 1);
-DELETE FROM public.sales_transaction(
-    WHERE transactionid = 1);
-DELETE FROM public.soldvia(
-    WHERE transactionid = 1);
-DELETE FROM public.view(
-    WHERE customerid = 1);
-DELETE FROM public.warehouse(
-    WHERE storeid = 1);
