dd791a39c8de825501f348ddbc1cb40e45d59661
Kyle <kyleham@byu.edu>
2021-02-03 18:53:06 -0700

    edit to make script work

--- views.sql
+++ views.sql
@@ -1,4 +1,4 @@
 /*This view will make it easy to group items by their location. 
 From here, we can easily pull information about a different product.*/
-SELECT productname FROM product
-GROUP BY productlocation;
+SELECT productlocation,  productname FROM product
+GROUP BY productlocation, productname;
