ef45c76bb821e8cde3de6209cf257cd8a8828def
Kyle <kyleham@byu.edu>
2021-02-03 18:54:04 -0700

    edit comment

--- views.sql
+++ views.sql
@@ -1,4 +1,5 @@
 /*This view will make it easy to group items by their location. 
-From here, we can easily pull information about a different product.*/
+From here, we can easily pull information about a different product.
+Saved as location_group_order*/
 SELECT productlocation,  productname FROM product
 GROUP BY productlocation, productname;
