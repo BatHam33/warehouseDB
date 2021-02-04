fd77e00bdd9b33c7f3225da77e4e1bb9b8f71fd4
Kyle <kyleham@byu.edu>
2021-02-03 18:46:11 -0700

    Added view about products and their locations

---
+++ views.sql
@@ -0,0 +1,4 @@
+/*This view will make it easy to group items by their location. 
+From here, we can easily pull information about a different product.*/
+SELECT productname FROM product
+GROUP BY productlocation;
