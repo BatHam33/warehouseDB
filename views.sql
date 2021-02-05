b1dc21eb6669469ea7cde531e34946f706ef6974
Kyle <kyleham@byu.edu>
2021-02-04 17:12:08 -0700

    Added new views for each product category

--- views.sql
+++ views.sql
@@ -3,3 +3,34 @@ From here, we can easily pull information about a different product.
 Saved as location_group_order*/
 SELECT productlocation,  productname FROM product
 GROUP BY productlocation, productname;
+
+/*The following views will all all items from a given category to be agregated.
+list_all_food*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='food';
+
+/*list_all_furniture*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='furniture';
+
+/*list_all_clothing*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='clothing';
+
+/*list_all_auto*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='auto';
+
+/*list_all_garden*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='garden';
+
+/*list_all_pharmacy*/
+SELECT productname, productprice, categoryname FROM product
+INNER JOIN category ON category.categoryid=product.categoryid
+WHERE categoryname='pharmacy';
