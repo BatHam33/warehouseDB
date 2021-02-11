0ea8598ae4a79fcc0f11db576e3c516eada3ff77
Kyle <kyleham@byu.edu>
2021-02-11 16:05:00 -0700

    added delete products from store

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,37 @@
-CREATE TRIGGER delete_Admin
+/*function and trigger to delete related admin account when when a user account is deleted*/
+CREATE OR REPLACE FUNCTION remove_old_admin()
+  RETURNS trigger AS
+$$
+BEGIN
+    DELETE FROM admin
+    WHERE admin.employeeid = OLD.employeeid;
+RETURN NEW;
+END;
+
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER delete_admin
     AFTER DELETE 
     ON employee
     FOR EACH ROW 
-    BEGIN
-    DELETE FROM admin
-        WHERE admin.employeeid = old.employeeid
-    end
+    EXECUTE PROCEDURE remove_old_admin();
 
+/*trigger to delete products from a store when the store is deleted*/
+CREATE OR REPLACE FUNCTION remove_store_prods()
+  RETURNS trigger AS
+$$
+BEGIN
+        DELETE FROM contains
+        WHERE contains.storeid=OLD.storeid;
+RETURN NEW;
+END;
+
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER delete_products
+    AFTER DELETE 
+    ON warehouse
+    FOR EACH ROW 
+    EXECUTE PROCEDURE remove_store_prods();
