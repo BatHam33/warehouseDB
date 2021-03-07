b6f3a26f0a33131b50a18cf81d7ebd8371f2cf76
Kyle <kyleham@byu.edu>
2021-03-07 00:33:08 -0700

    added Login.vue

--- vue.js/App.vue
+++ vue.js/App.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div id="app">
     <b-container>
---
+++ vue.js/pages/Admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar>
+      <b-navbar-brand :to="{ path: '/' }">Warehouse </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <router-link to="admin/adminlist" tag="b-button" class="float-left"
+      >ListofAdmins</router-link
+    >
+          <b-nav-item :to="{ path: '/logout' }">Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <br />
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Admin",
+};
+</script>
